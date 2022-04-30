4b71f2aa8c96912517c6674c2a3e4a5e14155bc4
UCIX210 <javier.uc@outlook.com>
2022-04-29 21:12:27 -0500

    orgien try1

---
+++ 0x04-Math_Gauss_elimination/0-gaussian_elimination.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def gaussian_elimination(A, b):
+    try:
+        return (np.linalg.solve(A, b))
+    except np.linalg.LinAlgError:
+        print("You can't divide by zero!")
+        return None
